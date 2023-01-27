19df36f531e4f2ffc1128f6c3ad6f2228bdb0756
RadwaMohammedKamal <120174338+RadwaMohammedKamal@users.noreply.github.com>
2023-01-28 01:50:18 +0200

    sec

--- lib/view/screens/home/azkarscreen.dart
+++ lib/view/screens/home/azkarscreen.dart
@@ -6,7 +6,7 @@ class AzkarScreen extends StatefulWidget {
   @override
   State<AzkarScreen> createState() => _AzkarScreenState();
 }
-
+//
 class _AzkarScreenState extends State<AzkarScreen> {
   @override
   Widget build(BuildContext context) {
